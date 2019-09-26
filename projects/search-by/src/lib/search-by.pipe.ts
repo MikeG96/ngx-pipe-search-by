d3a3db1864e06879fbc7271ef9aed0b63b3db364
Miguel Lopez <miguel.lopez@grupojaque.com>
2019-09-26 17:23:01 -0500

    (FEAT): Search in object sub-key

--- projects/search-by/src/lib/search-by.pipe.ts
+++ projects/search-by/src/lib/search-by.pipe.ts
@@ -11,14 +11,16 @@ export class SearchByPipe implements PipeTransform {
       if (searchWord !== '' && searchWord !== null) {
         if (sensitive) {
           value.map(val => {
-            if ((val[propertySearch]).toLowerCase().indexOf(searchWord.toLowerCase()) > -1) {
+            const result = this.searchInByString(val, propertySearch);
+            if (result.toLowerCase().indexOf(searchWord.toLowerCase()) > -1) {
               array.push(val);
             }
           });
           return array;
         } else {
           value.map(val => {
-            if ((val[propertySearch]).indexOf(searchWord) > -1) {
+            const result = this.searchInByString(val, propertySearch);
+            if (result.indexOf(searchWord) > -1) {
               array.push(val);
             }
           });
@@ -27,5 +29,19 @@ export class SearchByPipe implements PipeTransform {
       }
     }
     return value;
+  }
+
+  private searchInByString(object: {} = {}, property: string): any {
+    let key = property;
+    let obj = object;
+    key = key.replace(/\[(\w+)\]/g, '.$1'); // Convert indexes to properties
+    key = key.replace(/^\./, '');           // Strip a leading dot
+    const keys = key.split('.');
+    for (const attribute of keys) {
+      if (attribute in obj) {
+        obj = obj[attribute];
+      }
+    }
+    return obj;
   }
 }
